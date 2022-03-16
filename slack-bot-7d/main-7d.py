c0ad9b8c120f09b8eea2119c106ad5d3a42bb044
tutacrypto <tutal@protonmail.com>
2022-03-16 19:31:58 +0100

    update 7d: changed Dockerfile + requirements, removed image

--- slack-bot-7d/main-7d.py
+++ slack-bot-7d/main-7d.py
@@ -3,8 +3,6 @@ from dotenv import load_dotenv
 import pandas as pd
 from pycoingecko import CoinGeckoAPI
 from slack_sdk import WebClient
-import plotly.graph_objects as go
-
 
 # loading the .env file
 load_dotenv()
@@ -35,93 +33,62 @@ def get_coin_data(ordering):
 
 
 
-
-def plot_data(file_type, data):
-    df = data[:15]
-    df = df.sort_values('change 7d vs usd', ascending=True)
-
-    # plotting the dataframe
-    ids = df['---symbol---']
-    fig = go.Figure(data=[
-        go.Bar(name='7d', x=df['change 7d vs usd'], y=ids, orientation='h')
-    ])
-    # changing the bar mode
-    fig.update_layout(barmode='group')
-
-    if file_type == "html":
-        # save as an html file (interactive)
-        fig.write_html("html/file.html")
-    else:
-        # save as a picture
-        fig.write_image("images/fig1.png")
-
-
-
-def send_to_slack(type, top, bottom):
+def send_to_slack(top, bottom):
     client = WebClient(token=os.environ.get("SLACK_TOKEN"))
     channel = os.environ.get("CHANNEL")
 
 
-    if type == "image":
-        plot_data(top, "png")
-        client.files_upload(
-            channels=channel, 
-            initial_comment="Here's the 24h ranking of altcoins with their 7 days data, in BTC terms :rocket:", 
-            file="images/fig1.png",
-        )
-
-    elif type == "text":
-        client.chat_postMessage(
-            channel=channel,
-            username="The Crypto Bot",
-            # this is what will display in the notification
-            text= "Crypto Market Weekly Performance (USD)",
-            blocks=[
-            {
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": "*Top & bottom 15 tokens weekly performance (USD)*",
-                },
+    client.chat_postMessage(
+        channel=channel,
+        username="The Crypto Bot",
+        # this is what will display in the notification
+        text= "Crypto Market Weekly Performance (USD)",
+        blocks=[
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": "*Top & bottom 15 tokens weekly performance (USD)*",
             },
-            {
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": "Weekly performance of the crypto market. Best & worst 15 performing tokens. \n Hope it helps! :ok_hand: \n\n",
-                },
+        },
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": "Weekly performance of the crypto market. Best & worst 15 performing tokens. \n Hope it helps! :ok_hand: \n\n",
             },
-            {
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": "*Top 15*",
-                },
+        },
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": "*Top 15*",
             },
-            {
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": top.to_markdown(),
-                },
+        },
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": top.to_markdown(),
             },
-            {
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": "*Bottom 15*",
-                },
+        },
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": "*Bottom 15*",
             },
-            {
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": bottom.to_markdown(),
-                },
+        },
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": bottom.to_markdown(),
             },
-            
-            ]
-        )
+        },
+        
+        ]
+    )
 
 
 
@@ -129,5 +96,5 @@ def send_to_slack(type, top, bottom):
 if __name__ == '__main__':
     top = "top"
     bottom = "bottom"
-    send_to_slack("text", get_coin_data(top), get_coin_data(bottom))
+    send_to_slack(get_coin_data(top), get_coin_data(bottom))
 
